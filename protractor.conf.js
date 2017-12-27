31165bac8e8141fdf5b5c0dcc5f29492b736827c
PierreDuc <poulkruijt@gmail.com>
2017-12-27 10:27:27 +0200

    Added travis ci support

--- protractor.conf.js
+++ protractor.conf.js
@@ -9,7 +9,6 @@ exports.config = {
         'browserName': 'chrome'
     },
     directConnect: true,
-    baseUrl: 'http://localhost:4200/',
     framework: 'jasmine',
     jasmineNodeOpts: {
         showColors: true,
